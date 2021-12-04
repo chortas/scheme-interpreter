245e05abdc400e175287345b71c4b6971ae26b11
chortas <chortas@fi.uba.ar>
2021-12-04 11:57:41 -0300

    Add tests for verificar-parentesis

--- tp/test/tp/core_test.clj
+++ tp/test/tp/core_test.clj
@@ -2,6 +2,22 @@
   (:require [clojure.test :refer :all]
             [tp.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest verificar-parentesis-1-test
+  (testing "Un parentesis"
+    (is (= 1 (verificar-parentesis "(hola 'mundo")))))
+
+(deftest verificar-parentesis-2-test
+  (testing "Parentesis desbalanceados por uno"
+    (is (= -1 (verificar-parentesis "(hola '(mundo)))")))))
+
+(deftest verificar-parentesis-3-test
+  (testing "Parentesis desbalanceados por más de uno"
+    (is (= -1 (verificar-parentesis "(hola '(mundo) () 6) 7)")))))
+
+(deftest verificar-parentesis-4-test
+  (testing "Parentesis desbalanceados por más de uno"
+    (is (= -1 (verificar-parentesis "(hola '(mundo) () 6) 7) 9)")))))
+
+(deftest verificar-parentesis-5-test
+  (testing "Parentesis balanceados"
+    (is (= 0 (verificar-parentesis "(hola '(mundo) )")))))
